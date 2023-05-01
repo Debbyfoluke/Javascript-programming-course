f2f2813d82303f256df7189a39227107266244bf
Debbyfoluke <ogundipedeborah04@gmail.com>
2023-05-01 09:00:34 +0100

    commit the javascript programming course taking

---
+++ index.js
@@ -0,0 +1,120 @@
+// let header = document.getElementById("count-el")  
+
+// header.addEventListener(
+//     "mousehover",
+//     ()=>{
+//         header.style.color = "red"
+
+//     }
+// )
+
+// // let humanBeing 
+
+// // console.log(humanBeing)
+
+// let myAge =35
+// let humanDogRatio =7
+
+// let myDogAge =myAge * humanDogRatio
+
+// console.log(myDogAge)
+
+// // alert("Hello oooo")
+
+// let bonusPoints = 50
+// console.log(bonusPoints)
+
+// bonusPoints= bonuspoints = bonusPoints + 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+// function increment(){
+//     console.log("The button was clicked")
+        
+//     }
+
+
+
+// function countdown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+
+// //setting up the race
+
+// countdown()
+
+// //GO!
+// //players are running the race
+// //Race is finished
+
+// //Get ready for a new race
+
+// countdown()
+
+
+
+// function myLogger() {
+//     console.log(42)
+// }
+   
+// myLogger()
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// function logLapTime() {
+//     let totalTime = lap1 + lap2 + lap3
+//     console.log(totalTime)
+// }
+
+// let lapsCompleted = 0
+
+// function incrementLap() {
+//     lapsCompleted =lapsCompleted + 1   
+// }
+
+// incrementLap()
+// incrementLap()
+// incrementLap()
+
+// console.log(lapsCompleted)
+
+
+function increment() {
+    console.log("clicked")
+}
+
+
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+    console.log(count)
+}
+
+
+
+
+
+
+
+
+
+
+
+
